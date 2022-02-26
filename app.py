c9a7dd89a25663a6413d2cd0f1b87d3d5ef8004a
Jared Garlick <jared.b.garlick@gmail.com>
2022-02-26 12:42:07 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,341 @@
+import queue
+import networkx as nx
+from matplotlib import pyplot as plt
+import streamlit as st
+import plotly.express as px
+import random
+
+class Graph:
+    """A graph object, stored as an adjacency dictionary. Each node in the
+    graph is a key in the dictionary. The value of each key is a set of
+    the corresponding node's neighbors.
+
+    Attributes:
+        d (dict): the adjacency dictionary of the graph.
+    """
+    def __init__(self, adjacency={}):
+        """Store the adjacency dictionary as a class attribute"""
+        self.d = dict(adjacency)
+
+    def __str__(self):
+        """String representation: a view of the adjacency dictionary."""
+        return str(self.d)
+
+
+    def add_node(self, n):
+        """Add n to the graph (with no initial edges) if it is not already
+        present.
+
+        Parameters:
+            n: the label for the new node.
+        """
+        # Create a set of the nodes in the graph
+        node_set = set(self.d.keys())
+        if n not in node_set:
+            # Add the node n to the graph, with no initial edges, initialized as an empty set
+            self.d[n] = set()
+
+
+    def add_edge(self, u, v):
+        """Add an edge between node u and node v. Also add u and v to the graph
+        if they are not already present.
+
+        Parameters:
+            u: a node label.
+            v: a node label.
+        """
+        # Calling add_node() will add the node to the graph if it's not already present, will do nothing if it is in the graph already
+        self.add_node(u)
+        self.add_node(v)
+        # Add an edge between node u and node v
+        self.d[u].add(v)
+        self.d[v].add(u)
+
+
+    def remove_node(self, n):
+        """Remove n from the graph, including all edges adjacent to it.
+
+        Parameters:
+            n: the label for the node to remove.
+
+        Raises:
+            KeyError: if n is not in the graph.
+        """
+        # If the input value is not a node in the graph, raise a key error
+        if n not in self.d:
+            raise KeyError('The node is not in the graph.')
+        # Remove the node
+        del self.d[n]
+        # Remove all edges adjacent to the node
+        for value in self.d.values():
+            if n in value:
+                value.remove(n)
+
+
+    def remove_edge(self, u, v):
+        """Remove the edge between nodes u and v.
+
+        Parameters:
+            u: a node label.
+            v: a node label.
+
+        Raises:
+            KeyError: if u or v are not in the graph, or if there is no
+                edge between u and v.
+        """
+        # Raise a key error if u or v are not in the graph, or if there is no edge between u and v
+        if u not in self.d or v not in self.d:
+            raise KeyError('Either u or v is not in the graph.')
+        if u not in self.d[v] or v not in self.d[u]:
+            raise KeyError('There is not edge between u and v.')
+
+        # Remove edges between u and v
+        self.d[v].discard(u)
+        self.d[u].discard(v)
+
+
+    def traverse(self, source):
+        """Traverse the graph with a breadth-first search until all nodes
+        have been visited. Return the list of nodes in the order that they
+        were visited.
+
+        Parameters:
+            source: the node to start the search at.
+
+        Returns:
+            (list): the nodes in order of visitation.
+
+        Raises:
+            KeyError: if the source node is not in the graph.
+        """
+        visited_nodes = [] # A list for the nodes that have been visited
+        order_visited = queue.Queue() # A queue for the nodes that will be visited in the order they were discovered
+        marked_visited = set() # A set for the nodes that have been visited, or that are marked to be visited
+
+        # If the source node is not in the dictionary, raise a key error
+        if source not in self.d:
+            raise KeyError('The source node is not in the graph.')
+
+        # Add the source node to the queue and set of visited nodes
+        order_visited.put(source)
+        marked_visited.add(source)
+
+        # Traverse the nodes until the queue is empty
+        while not order_visited.empty():
+            # Pop a node off of the queue, call it the current node
+            current_node = order_visited.get()
+            # Append the current node to the list of visited nodes
+            visited_nodes.append(current_node)
+            # Add the neighbors of the current node that are not in M to Q and M
+            for val in self.d[current_node]:
+                if val not in marked_visited:
+                    order_visited.put(val)
+                    marked_visited.add(val)
+        # Return the list of nodes in order of visitation
+        return visited_nodes
+
+
+    def shortest_path(self, source, target):
+        """Begin a BFS at the source node and proceed until the target is
+        found. Return a list containing the nodes in the shortest path from
+        the source to the target, including endoints.
+
+        Parameters:
+            source: the node to start the search at.
+            target: the node to search for.
+
+        Returns:
+            A list of nodes along the shortest path from source to target,
+                including the endpoints.
+
+        Raises:
+            KeyError: if the source or target nodes are not in the graph.
+        """
+        # Raise an error if the source or target nodes are not in the graph
+        if source not in self.d or target not in self.d:
+            raise KeyError('The source or target nodes are not in the graph.')
+
+        visited_nodes = [] # A list for the nodes that have been visited
+        order_visited = queue.Queue() # A queue for the order that the nodes have been discovered
+        marked_visited = set() # A set of the nodes that have been visited, or that are marked to be visited
+
+        # Add the source node to the queue and set of visited nodes
+        order_visited.put(source)
+        marked_visited.add(source)
+
+        # Traverse the nodes until the target node is in the list of visited nodes
+        while target not in visited_nodes:
+            # Pop a node off of the queue, call it our current node
+            current_node = order_visited.get()
+            # Visit the current node by appending it to V
+            visited_nodes.append(current_node)
+            # Add the neighbors of the current node that are not in M to Q and M
+            for val in self.d[current_node]:
+                if val not in marked_visited:
+                    order_visited.put(val)
+                    marked_visited.add(val)
+        # Return the list of the path from the source to target, including the endpoints
+        return visited_nodes
+
+
+class MovieGraph:
+    """Class for solving the Kevin Bacon problem with movie data from IMDb."""
+
+    def __init__(self, filename="movie_data.txt"):
+        """Initialize a set for movie titles, a set for actor names, and an
+        empty NetworkX Graph, and store them as attributes. Read the speficied
+        file line by line, adding the title to the set of movies and the cast
+        members to the set of actors. Add an edge to the graph between the
+        movie and each cast member.
+
+        Each line of the file represents one movie: the title is listed first,
+        then the cast members, with entries separated by a '/' character.
+        For example, the line for 'The Dark Knight (2008)' starts with
+
+        The Dark Knight (2008)/Christian Bale/Heath Ledger/Aaron Eckhart/...
+
+        Any '/' characters in movie titles have been replaced with the
+        vertical pipe character | (for example, Frost|Nixon (2008)).
+        """
+        # Initialize the movie set and actor names set, as well as a nx graph object
+        self.movie_titles = set()
+        self.actor_names = set()
+        self.G = nx.Graph()
+        # Read the file and save the contents, close the file
+        inputfile = open(filename, 'r', encoding='utf-8')
+        lines = inputfile.readlines()
+        inputfile.close()
+        for line in lines:
+            # strip and split the lines, so that we have a list for each movie
+            movie_list = line.strip().split('/')
+            # Pop off the first element (the movie) so that the list becomes just actors
+            title = movie_list.pop(0)
+            # Add the movie title to the set of movie titles
+            self.movie_titles.add(title)
+            # Go thorugh each actor in the movie, add them to the set of actors, add an edge between the movie and each actor
+            for i in movie_list:
+                self.actor_names.add(i)
+                self.G.add_edge(title, i)
+
+
+    def path_to_actor(self, source, target):
+        """Compute the shortest path from source to target and the degrees of
+        separation between source and target.
+
+        Returns:
+            (list): a shortest path from source to target, including endpoints and movies.
+            (int): the number of steps from source to target, excluding movies.
+        """
+        # Create an nx list of the shortest path from source to target
+        shortest_path = nx.shortest_path(self.G, source, target)
+        # Empty list to store just the actor path
+        actor_path = []
+        for i in shortest_path:
+            # Only store the element of shortest path list that are actors
+            if i not in self.movie_titles:
+                actor_path.append(i)
+        # Return the shortest path from source to target, including endpoints and movies, as well as the number of steps from source to target, excluding movies
+        return shortest_path, len(actor_path)-1
+
+
+    def average_number(self, target):
+        """Calculate the shortest path lengths of every actor to the target
+        (not including movies). Plot the distribution of path lengths and
+        return the average path length.
+
+        Returns:
+            (float): the average path length from actor to target.
+        """
+        # Dictionary of all paths between every actor and target
+        all_paths = nx.shortest_path_length(self.G, target=target)
+        # Empty list for the lengths of paths
+        values = []
+        # Go through each key in the dictionary
+        for value in all_paths.keys():
+            # Only consider the paths to the actors, not the paths to movies
+            if all_paths[value] % 2 == 0:
+                # Add the length of the path, divided by two in order to just get the actors, not the movies
+                values.append(all_paths[value] // 2)
+        # Create a histogram of the path lengths between all actors and the target
+        # plt.hist(values, bins=[i-.5 for i in range(8)])
+        # plt.title('Average path lengths of every actor to ' + str(target))
+        # plt.show()
+        fig, ax = plt.subplots()
+        ax.hist(values, bins=[i-.5 for i in range(8)])
+        ax.set_title('Average path lengths of every actor to ' + str(target))
+        # Return the average path length from all actors to the target actor
+        return fig, sum(values)/len(values)
+
+
+
+
+
+
+
+
+st.title('The Kevin Bacon Problem')
+st.subheader('A fun way to learn about graph theory.')
+st.write('Have you ever heard about the six degrees of Kevin Bacon?')
+st.write('Basically, the idea is that every actor in hollywood is typically connected to Kevin Bacon by 6 movies or less.')
+st.write('Think of it like this: ')
+st.markdown('Kevin Bacon was in _Patriots Day (2016)_')
+st.markdown('Mark Falvo was also in _Patriots Day_, and was also in _Captain America: Civil War (2016)_.')
+st.markdown('Tom Holland was in _Captain America: Civil War_.')
+st.write('So, Kevin Bacon and Tom Holland are just one actor away from each other, giving Tom Holland a Bacon Score of 2')
+st.write('Try it yourself! Click the button below to select randomly or type in two actors\' names and see how they\'re connected.')
+
+obj = MovieGraph('movie_data_small.txt')
+actor_list = obj.actor_names
+button2 = st.button('Choose random actors')
+col1, col2 = st.columns(2)
+
+with col1:
+    actor1 = st.text_input('Input the first actor: ')
+
+with col2:
+    actor2 = st.text_input('Input another actor: ')
+
+button1 = st.button('Run')
+st.markdown('_Running the algorithm might take a minute._')
+
+
+if button1:
+    path, score = obj.path_to_actor(actor1, actor2)
+    st.write(path)
+    st.write("Score: ", score)
+    st.write("Shortly, two graphs will appear showing the distribution of scores for the selected actors.")
+    st.write("Basically, this shows how many actors are in the same movies, two movies away, etc.")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        fig, average = obj.average_number(actor1)
+        st.pyplot(fig)
+        st.text('Average score: ' + str(average))
+
+    with col2:
+        fig2, average2 = obj.average_number(actor2)
+        st.pyplot(fig2)
+        st.text('Average score: ' + str(average2))
+    
+if button2:
+    actor1 = random.choice(list(actor_list))
+    actor2 = random.choice(list(actor_list))
+    path, score = obj.path_to_actor(actor1, actor2)
+    st.write('Actors selected: ' + str(actor1) + " & " + str(actor2))
+    st.write(path)
+    st.write("Score: ", score)
+    st.write("Shortly, two graphs will appear showing the distribution of scores for the selected actors.")
+    st.write("Basically, this shows how many actors are in the same movies, two movies away, etc.")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        fig, average = obj.average_number(actor1)
+        st.pyplot(fig)
+
+    with col2:
+        fig, average = obj.average_number(actor2)
+        st.pyplot(fig)
+
+
+
